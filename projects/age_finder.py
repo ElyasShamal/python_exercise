5f2c2e4ded6ba79e89f47bfe977b7dc2af14dbdb
Elyas <Elyas.shamal2@gmail.com>
2023-08-04 19:06:18 -0400

    age finder

---
+++ projects/age_finder.py
@@ -0,0 +1,6 @@
+# write a program that take the user birth year  and return his age
+
+full_name = input('what is your name ? ')
+birth_year = int(input('what is your birth year ? '))
+result = 2023 - birth_year
+print(f'Hi, {full_name} your {result} years old')
